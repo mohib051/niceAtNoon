19bd568396e6b0fc4c08d2075d0e7300e5bf6ca3
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2024-12-07 00:07:53 +0530

    made page2 with transition effect

--- niceAtNoon/src/App.jsx
+++ niceAtNoon/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Section1 from "./components/section1/Section1";
 import ImageSection from "./components/ImageSection";
 import ImageBottom from "./components/ImageBottom";
+import Page2 from "./components/Page2";
 const App = () => {
   return (
     <div>
@@ -9,6 +10,7 @@ const App = () => {
        <Section1/>
        <ImageSection/>
        <ImageBottom/>
+       <Page2/>
     </div>
   )
 }
