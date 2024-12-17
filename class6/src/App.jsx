35fe694eb67e014ec8419e952130dd27ea9eb310
Kazi Mohibuzzaman <kazimohib51@gmail.com>
2024-12-17 19:07:40 +0530

    created form to take inputs and show details in card and added delete button to delete respective card

--- class6/src/App.jsx
+++ class6/src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [empid, setempid] = useState("");
+  const [imgurl, setimgurl] = useState("");
+
+  const [allusers, setallusers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+
+    const newarr = [...allusers, { name, email, empid, imgurl }];
+    setallusers(newarr);
+
+    setname("");
+    setemail("");
+    setempid("");
+    setimgurl("");
+  };
+  const handleDelete = (index)=>{
+    const copyUser =[...allusers]
+      copyUser.splice(index,1)
+      setallusers(copyUser)
+    console.log("delte")
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit} className="p-3  flex gap-3" action="">
+        <input
+          value={name}
+          onChange={(e) => setname(e.target.value)}
+          className="border-2 border-black p-2"
+          type="text"
+          placeholder="enter your name"
+        />
 
-export default App
+        <input
+          value={email}
+          onChange={(e) => setemail(e.target.value)}
+          className="border-2 border-black p-2"
+          type="text"
+          placeholder="enter your email"
+        />
+        <input
+          value={imgurl}
+          onChange={(e) => setimgurl(e.target.value)}
+          className="border-2 border-black p-2"
+          type="text"
+          placeholder="enter image url"
+        />
+
+        <input
+          value={empid}
+          onChange={(e) => setempid(e.target.value)}
+          className="border-2 border-black p-2"
+          type="text"
+          placeholder="enter employee id"
+        />
+        <input className="bg-blue-200 p-3 cursor-pointer" type="submit" />
+      </form>
+
+      {allusers.map((item, index) => (
+        <div
+          key={index}
+          id="card-div"
+          className="w-72 h-auto p-7 border-2 bg-slate-100 mx-auto flex flex-col  items-center gap-5 mt-14 "
+        >
+          <div id="img-div" className=" w-36 h-36">
+            <img
+              className=" w-full h-full rounded-full"
+              src={item.imgurl}
+              alt=""
+            />
+          </div>
+          <h1 className="font-bold text-2xl uppercase">{item.name}</h1>
+          <h1 className="font-semibold text-1xl">{item.email}</h1>
+          <h1 className="font-semibold text-2xl uppercase">{item.empid}</h1>
+          <button onClick={()=>handleDelete(index)} className="bg-red-400 p-3">Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
